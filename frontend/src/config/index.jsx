bb98f8eac2f250f1c840c9d2dbf6fdeeb9c2e6ee
Ram krishna <goluupadhyay600@gmail.com>
2025-10-13 11:35:27 +0530

    Add the url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,8 +1,8 @@
 const { default: axios } = require("axios");
 
 
-export const BASE_URL = "http://localhost:9080"
-
+// export const BASE_URL = "http://localhost:9080"
+export const BASE_URL = "https://linkdinclone-xaat.onrender.com"
 export const clientServer = axios.create({
 baseURL: BASE_URL,
 })
